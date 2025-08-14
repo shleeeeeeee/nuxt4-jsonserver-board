833ae3930aae4b7501de8f5c1afc5e2a1fee8fe1
이제연 <jylee@idevel.kr>
2025-08-14 11:19:12 +0900

    ✨ feat: 게시판 미션 기본 템플릿 구현

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,5 @@
+import antfu from '@antfu/eslint-config'
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(antfu())
---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+/* global defineNuxtConfig */
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@primevue/nuxt-module'],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+})
